fcc941bd8c10bbb375e31447bf9539493d3d97fa
Maxym Kolesnykov <maxkolesnikov93@gmail.com>
2017-07-31 19:36:08 +0300

    added rental api test

---
+++ tests/Feature/RentalApiTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maksym
+ * Date: 31.07.17
+ * Time: 19:10
+ */
+
+namespace Tests\Feature;
+
+use App\Services\RentalService;
+use Faker\Factory;
+use Illuminate\Support\Facades\Artisan;
+use Tests\BookingsHelper;
+use Tests\TestCase;
+
+class RentalApiTest extends TestCase
+{
+    /**
+     * BookingsHelper trait
+     */
+    use BookingsHelper;
+
+    /**
+     * Set up RentalServiceTest
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call("migrate:refresh");
+    }
+
+    /**
+     * Test rent car by api route
+     */
+    public function testRentCar()
+    {
+        $user = $this->createUser();
+        $car = $this->createCar();
+        $rentedFrom = Factory::create()->address;
+
+        $this->actingAs($user)
+            ->json('POST', '/api/cars/rent/', [
+                'user' => $user->id,
+                'car' => $car->id,
+                'rented_from' => $rentedFrom
+            ])
+            ->assertStatus(200);
+    }
+}
